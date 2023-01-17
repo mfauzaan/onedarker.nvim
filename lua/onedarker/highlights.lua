8452cbe62ba7e5f67df49c8278c717f5e3f4f11b
Fauzaan <mfauzaan@icloud.com>
2023-01-17 08:19:40 +0000

    chore: updated color

--- lua/onedarker/highlights.lua
+++ lua/onedarker/highlights.lua
@@ -24,7 +24,7 @@ local common = {
   LineNr = { fg = C.context },
   FloatBorder = { fg = C.gray, bg = C.alt_bg },
   Whitespace = { fg = C.gray },
-  VertSplit = { fg = C.alt_bg, bg = C.alt_bg },
+  VertSplit = { fg = C.none, bg = C.alt_bg },
   CursorLine = { bg = C.dark },
   CursorColumn = { bg = C.dark },
   ColorColumn = { bg = C.dark },
